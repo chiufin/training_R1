f8f1b3ba98825dc534a63659c87b99fc4cb4f059
stacy <chiufin@hotmail.com>
2018-09-25 00:05:02 +0800

    remove bin from gitignore

--- portal/routes/login.js
+++ portal/routes/login.js
@@ -5,4 +5,16 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Log in' });
 });
 
+// send form
+router.post('/post', function(req, res){
+  console.log('login post api?')
+})
+
+//logout
+router.get('/logout', function(req, res){
+  console.log('logout api?')
+})
+
+
+
 module.exports = router;
