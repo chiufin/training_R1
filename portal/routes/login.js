6f20682df1c96e8434647639cbe2fad02a8f5f99
chiufin <chiufin@hotmail.com>
2018-09-25 14:53:45 +0800

    set cookie when login successfully, also show welcome in login page

--- portal/routes/login.js
+++ portal/routes/login.js
@@ -2,17 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Log in' });
+  let ifLogin = false
+  if(req.signedCookies.account && req.signedCookies.password){
+    ifLogin = true
+  }
+  res.render('login', { ifLogin });
 });
 
 // send form
 router.post('/post', function(req, res){
-  console.log('login post api?')
+  if(req.body.account == '' || req.body.password== ''){
+    return res.redirect('/login')
+  }else{
+    //set cookie
+    res.cookie('account', req.body.account, { path: '/', signed: true, maxAge:60000})
+    res.cookie('password', req.body.password, { path: '/', signed: true, maxAge:60000})
+    return res.redirect('/users')
+  }
 })
 
 //logout
 router.get('/logout', function(req, res){
-  console.log('logout api?')
+  res.clearCookie('account',{path:'/'});
+  res.clearCookie('password',{path:'/'});
 })
 
 
--- portal/routes/users.js
+++ portal/routes/users.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('users', {users: [
-    
-  ]});
+  console.log(req.signedCookies.account)
+  console.log(req.signedCookies.password)
+  // if(req.signedCookies.account && req.signedCookies.password){
+  //   res.redirect('/login')
+  // }else{
+    res.render('users', {users: []});
+  // }
 });
 
 module.exports = router;
