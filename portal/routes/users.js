ff4bf63ca989feda445f8bcfae6e7325d908688b
stacy <chiufin@hotmail.com>
2018-09-26 22:18:40 +0800

    update user style

--- portal/routes/users.js
+++ portal/routes/users.js
@@ -5,7 +5,7 @@ router.get('/', function(req, res, next) {
   console.log(req.signedCookies.account)
   console.log(req.signedCookies.password)
   if(req.signedCookies.account && req.signedCookies.password){
-    res.render('users', {users: ['ken', 'wilson', 'jacob', 'jake', 'stacy']});  
+    res.render('users', {userName:'stacy' ,users: ['ken', 'wilson', 'jacob', 'jake', 'stacy']});  
   }else{
     res.redirect('/login')
   }
