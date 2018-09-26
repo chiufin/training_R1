90e649f268805c3da74917d4cb74e33c7033a50c
chiufin <chiufin@hotmail.com>
2018-09-26 17:20:58 +0800

    add create page modal

---
+++ portal/public/js/app.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    $("#createUser").click(function() {
+        console.log('show panel')
+        
+    });
+
+    $("#closeUserPanel").click(function() {
+        
+    });
+});
--- portal/routes/users.js
+++ portal/routes/users.js
@@ -5,10 +5,14 @@ router.get('/', function(req, res, next) {
   console.log(req.signedCookies.account)
   console.log(req.signedCookies.password)
   if(req.signedCookies.account && req.signedCookies.password){
-    res.render('users', {users: []});  
+    res.render('users', {users: ['ken', 'wilson', 'jacob', 'jake', 'stacy']});  
   }else{
     res.redirect('/login')
   }
+});
+
+router.get('/post', function(req, res, next) {
+  
 });
 
 module.exports = router;
