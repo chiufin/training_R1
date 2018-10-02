550def18e1e7ee4e682885fef1127ff0c11609d3
stacy <chiufin@hotmail.com>
2018-10-02 08:29:47 +0800

    read users

--- portal/app.js
+++ portal/app.js
@@ -9,25 +9,37 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login')
 
 var mysql = require('mysql');
+var myConnection  = require('express-myconnection')
 
-var connection = mysql.createConnection({
+var dbOptions = {
   host     : 'localhost',
   user     : 'root',
   password : 'password',
   database: 'users'
-});
+}
 
-connection.connect();
 
-connection.query('SELECT * FROM cats', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0]);
-});
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'password',
+//   database: 'users'
+// });
 
-connection.end();
+// connection.connect();
+
+// connection.query('SELECT * FROM cats', function(err, rows, fields) {
+//   if (err) throw err;
+//   console.log('The solution is: ', rows[0]);
+// });
+
+// connection.end();
 
 
 var app = express();
+
+
+app.use(myConnection(mysql, dbOptions, 'pool'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- portal/routes/users.js
+++ portal/routes/users.js
@@ -2,13 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(req.signedCookies.account)
-  console.log(req.signedCookies.password)
-  if(req.signedCookies.account && req.signedCookies.password){
-    res.render('users', {userName:'stacy' ,users: ['ken', 'wilson', 'jacob', 'jake', 'stacy']});  
-  }else{
-    res.redirect('/login')
-  }
+
+  // if(req.signedCookies.account && req.signedCookies.password){
+  //   res.render('users', {userName:'stacy' ,users: ['ken', 'wilson', 'jacob', 'jake', 'stacy']});  
+  // }else{
+  //   res.redirect('/login')
+  // }
+  req.getConnection(function(error, conn) {
+      conn.query('SELECT * FROM cats',function(err, rows, fields) {
+          //if(err) throw err
+          if (err) {
+              req.flash('error', err)
+              res.render('users', {userName:'stacy' ,users: ['opps']});  
+          } else {
+              // render to views/user/list.ejs template file
+              console.log(rows)
+              var list = []
+              for(var i = 0 ; i < rows.length ; i++){
+                list.push(rows[i].name)
+              }
+              res.render('users', {userName:'stacy' ,users: list});  
+              
+          }
+      })
+  })
+
+
 });
 
 router.get('/post', function(req, res, next) {
