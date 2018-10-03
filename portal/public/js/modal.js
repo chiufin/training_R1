22f0fe27465a29caf164e4f24d81d9421b6a41b0
chiufin <chiufin@hotmail.com>
2018-10-03 16:30:42 +0800

    separate client js

---
+++ portal/public/js/modal.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+
+    //modal
+    $("#createUser").click(function() {
+        $("#createUserModal").css({ display: 'block' })
+    });
+
+    $("#closeUserPanel").click(function() {
+        $("#createUserModal").css({ display: 'none' })
+    });
+
+    window.onclick = function(event) {
+        if (event.target.id == 'createUserModal') {
+            $("#createUserModal").css({ display: 'none' })
+        }
+    }
+
+});
--- portal/public/js/users.js
+++ portal/public/js/users.js
@@ -1,21 +1,5 @@
 $( document ).ready(function() {
 
-
-    //modal
-    $("#createUser").click(function() {
-        $("#createUserModal").css({ display: 'block' })
-    });
-
-    $("#closeUserPanel").click(function() {
-        $("#createUserModal").css({ display: 'none' })
-    });
-
-    window.onclick = function(event) {
-        if (event.target.id == 'createUserModal') {
-            $("#createUserModal").css({ display: 'none' })
-        }
-    }
-
     //edit user
     $("#users .edit").click(function() {
         var index = $("#users .edit").index(this)
