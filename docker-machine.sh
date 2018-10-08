77e4cd1c7a25a62d92e74f27a828844e61f1c3d5
chiufin <chiufin@hotmail.com>
2018-10-08 10:25:18 +0800

    update docker-machine start

--- docker-machine.sh
+++ docker-machine.sh
@@ -3,6 +3,10 @@
 # docker-machine default reset 
 # for window 7 use
 
+# docker-machine start
+
+# docker-machine env default
+
 export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST=tcp://192.168.99.100:2376
 export DOCKER_CERT_PATH="C:\Users\stacy.chen\.docker\machine\certs"
