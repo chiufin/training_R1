3175c791b07bbbd77f1d079b33a52f1025c44c1f
chiufin <chiufin@hotmail.com>
2018-10-11 10:53:12 +0800

    fix docker-machine script host ip

--- docker-machine.sh
+++ docker-machine.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# docker-machine default reset ...
 # for window 7 use
 
 # docker-machine start
-
-# docker-machine env default
 # docker-machine ls
-# docker-machine start
+# docker-machine rm default
+
+# docker-machine create --driver virtualbox default
+# docker-machine env default
 
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://localhost:2376
+export DOCKER_HOST=tcp://192.168.99.100:2376
 export DOCKER_CERT_PATH="C:\Users\stacy.chen\.docker\machine\certs"
 
 # docker-machine ls
