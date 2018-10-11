f1c6f7ccf118f2079e09897d2f0fbea2d1685043
chiufin <chiufin@hotmail.com>
2018-10-11 17:02:59 +0800

    login connect mysql

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -4,13 +4,14 @@ CREATE TABLE user
   name            VARCHAR(150) NOT NULL,
   email           VARCHAR(320) NOT NULL,
   psw             CHAR(32) NOT NULL,
-  -- created_time    TIMESTAMP NULL DEFAULT NULL,
+  -- created_time    TIMESTAMP NULL DEFAULT NULL,
   -- updated_time    TIMESTAMP NULL DEFAULT NULL,
+  login_status    TINYINT(1) NOT NULL,
   PRIMARY KEY     (id)                                 
 );
 
 
-INSERT INTO user ( name, email, psw ) VALUES
-  ( 'Stacy', 'stacy.chen@innovasolutions.com', 'stacy' ),
-  ( 'Wilson', 'wilson.chen@innovasolutions.com', 'wilson' ),
-  ( 'Jason', 'jason.chen@innvovasolutions.com', 'jason' );
+INSERT INTO user ( name, email, psw, login_status ) VALUES
+  ( 'Stacy', 'stacy.chen@innovasolutions.com', 'stacy', false ),
+  ( 'Wilson', 'wilson.chen@innovasolutions.com', 'wilson', false ),
+  ( 'Jason', 'jason.chen@innvovasolutions.com', 'jason', false );
--- portal/routes/login.js
+++ portal/routes/login.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db.js').connection;
 
 router.get('/', function(req, res, next) {
-  let ifLogin = false
   if(req.signedCookies.account && req.signedCookies.password){
-    ifLogin = true
+    try{
+      connection.query(`SELECT * FROM user WHERE email=${JSON.stringify(req.signedCookies.account)}`, function(err, result, fields) {
+        if (err) throw err; 
+        if(result[0].psw == req.signedCookies.password){
+          res.render('login', { ifLogin: true });
+        }
+      });
+    }catch(err){
+      console.log(err)
+    }
+  }else{
+    res.render('login', { ifLogin: false });
   }
-  res.render('login', { ifLogin });
 });
 
 module.exports = router;
