b91d233b50fd12256ed5da3cc56b008d284e7f17
chiufin <chiufin@hotmail.com>
2018-10-19 14:16:30 +0800

    show file list

--- portal/routes/filesPage.js
+++ portal/routes/filesPage.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+
 
 router.get('/', function(req, res, next) {
     if(req.session.name){
-        res.render('filesPage', {userName: req.session.name});  
+        fs.readdir('./uploads/', (err, files) => {
+            res.render('filesPage', {userName: req.session.name, files});  
+        })
     }else{
         return res.redirect('/login')
     }
