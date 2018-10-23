94e87589b2aff3741221c02bb01ecafa488bb49d
chiufin <chiufin@hotmail.com>
2018-10-23 14:36:29 +0800

    set session secret to env file

--- portal/app.js
+++ portal/app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var session = require('express-session')
 var bodyParser = require('body-parser')
 var logger = require('morgan');
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/usersPage');
 var filesRouter = require('./routes/filesPage');
 var loginRouter = require('./routes/loginPage')
 var apiRouter = require('./routes/api')
-
+require('dotenv').config()
 
 var app = express();
 const port = 3000
@@ -27,8 +27,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('123456789000'));
-app.use(session({secret:"123", resave:false, saveUninitialized:true}));
+// app.use(cookieParser('123456789000'));
+app.use(session({secret: process.env.SESSION_SECRET || '123', resave:false, saveUninitialized:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
