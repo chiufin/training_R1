7be1e3a0fb84e3f8261201f492df0c84ca539c13
chiufin <chiufin@hotmail.com>
2018-10-30 10:34:28 +0800

    remove unessary code in app.js

--- portal/app.js
+++ portal/app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
 var session = require('express-session')
 var bodyParser = require('body-parser')
 var logger = require('morgan');
@@ -16,18 +15,14 @@ require('dotenv').config()
 var app = express();
 const port = 3000
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 
-
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('123456789000'));
 app.use(session({secret: process.env.SESSION_SECRET || '123', resave:false, saveUninitialized:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -44,8 +39,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
