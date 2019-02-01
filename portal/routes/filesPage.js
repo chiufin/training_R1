85081e469c43e01aa6f0fc3d9f1a26ffd027aca1
chiufin <chiufin@hotmail.com>
2019-02-01 14:46:37 +0800

    read s3 list objects

--- portal/routes/filesPage.js
+++ portal/routes/filesPage.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const s3bucket = require('../config/s3.config.js');
 
 
 router.get('/', function(req, res, next) {
     if(req.session.name){
-        fs.readdir('./uploads/', (err, files) => {
-            res.render('filesPage', {userName: req.session.name, files});  
-        })
+        var params = { Bucket: 'stacy-upload-file'};
+        s3bucket.listObjects(params, function(err, data) {
+            if (err) {
+               console.log("Error", err);
+            } else {
+               res.render('filesPage', {userName: req.session.name, files: data.Contents});  
+            }
+         });
     }else{
         return res.redirect('/login')
     }
